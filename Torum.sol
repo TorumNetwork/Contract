556706e6af35edd4632675d99769be2278bc5315
Torum Token (XTM) <torumtokenxtm@gmail.com>
2025-04-26 02:50:35 -0700

    Torum.sol

--- Torum.sol
+++ Torum.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Torum is ERC20, Ownable {
     constructor(address initialOwner) ERC20("Torum Token", "XTM") Ownable(initialOwner) {
-        _mint(initialOwner, 1_000_000_000_000 * 1e18);
-    }
+        _mint(initialOwner, 200_000_000 * 1e18);}function mint(address to, uint256 amount) external onlyOwner {_mint(to, amount);}
+    function burn(uint256 amount) external {_burn(msg.sender, amount);}
 }
